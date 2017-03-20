feb508a12e026bd4d573095eac7125c8dd081299
Andrew Gaul <andrew@gaul.org>
2017-03-19 22:19:58 -0700

    Add support for du --human-readable

--- du/du.go
+++ du/du.go
@@ -14,13 +14,13 @@
 
 package main
 
-// TODO: -h, --human-readable
 // TODO: -x, --one-file-system
 // TODO: test symlinks
 
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"sync"
 	"sync/atomic"
 	"syscall"
@@ -31,6 +31,7 @@ import (
 
 // TODO: -k
 var blockSize = flag.IntP("block-size", "B", 1024, "scale sizes by SIZE before printing them")
+var humanReadable = flag.BoolP("human-readable", "h", false, "print sizes in human readable format (e.g., 1K 234M 2G)")
 var summarize = flag.BoolP("summarize", "s", false, "display only a total for each argument")
 
 var sem *semaphore.Semaphore
@@ -118,7 +119,7 @@ func du(name string, wgWg *sync.WaitGroup, sumSum *int64, err2 *error) error {
 	wg.Wait()
 	atomic.AddInt64(sumSum, atomic.LoadInt64(&sum))
 	if !*summarize {
-		fmt.Printf("%d\t%s\n", atomic.LoadInt64(&sum), name)
+		fmt.Printf("%s\t%s\n", formatBlocks(atomic.LoadInt64(&sum)), name)
 	}
 	return nil
 }
@@ -131,6 +132,39 @@ func scaleBlocks(blocks int64) int64 {
 	}
 }
 
+func formatBlocks(blocks int64) string {
+	if *humanReadable {
+		bytes := blocks * int64(*blockSize)
+		if bytes > (1 << 60) {
+			return fmt.Sprintf("%2.1fE", float64(bytes)/(1<<60))
+		} else if bytes > 10*(1<<50) {
+			return fmt.Sprintf("%dP", bytes/(1<<50))
+		} else if bytes > (1 << 50) {
+			return fmt.Sprintf("%2.1fP", float64(bytes)/(1<<50))
+		} else if bytes > 10*(1<<40) {
+			return fmt.Sprintf("%dT", bytes/(1<<40))
+		} else if bytes > (1 << 40) {
+			return fmt.Sprintf("%2.1fT", float64(bytes)/(1<<40))
+		} else if bytes > 10*(1<<30) {
+			return fmt.Sprintf("%dG", bytes/(1<<30))
+		} else if bytes > (1 << 30) {
+			return fmt.Sprintf("%2.1fG", float64(bytes)/(1<<30))
+		} else if bytes > 10*(1<<20) {
+			return fmt.Sprintf("%dM", bytes/(1<<20))
+		} else if bytes > (1 << 20) {
+			return fmt.Sprintf("%2.1fM", float64(bytes)/(1<<20))
+		} else if bytes > 10*(1<<10) {
+			return fmt.Sprintf("%dK", bytes/(1<<10))
+		} else if bytes > (1 << 10) {
+			return fmt.Sprintf("%2.1fK", float64(bytes)/(1<<10))
+		} else {
+			return strconv.FormatInt(bytes, 10)
+		}
+	} else {
+		return strconv.FormatInt(blocks, 10)
+	}
+}
+
 func main() {
 	flag.Parse()
 	args := flag.Args()
@@ -140,7 +174,7 @@ func main() {
 
 	err, sum := Du(args...)
 	if *summarize {
-		fmt.Printf("%d\t%s\n", sum, args[0])
+		fmt.Printf("%s\t%s\n", formatBlocks(sum), args[0])
 	}
 	if err != nil {
 		os.Exit(1)
